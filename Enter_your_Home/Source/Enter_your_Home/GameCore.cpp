cec9e88439ee8570646a0aaaea56c0ac13c4b58e
Nicolas Sobczak <nicolas.sobczak@orange.fr>
2018-05-13 18:12:57 +0200

    Setup for build + updated gitignore for build folder

--- Enter_your_Home/Source/Enter_your_Home/GameCore.cpp
+++ Enter_your_Home/Source/Enter_your_Home/GameCore.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GameCore.h"
-#include "Blueprint/UserWidget.h"
 
 
 // Sets default values for this component's properties
